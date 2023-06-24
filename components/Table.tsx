86703e75c3fc03345c6fe15b2623143794270319
puritybirir <puritybirir@gmail.com>
2023-06-24 18:40:07 +0300

    Create table and editable fields

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,32 +1,54 @@
+import Image from "next/image";
+import pencil from '../assets/images/pencil.svg';
+
 const Table = () => {
   const data = [
-    ['Value A1', 'Value B1'],
-    ['Value A2', 'Value B2'],
-    ['Value A3', 'Value B3'],
+    ['A1', 'B1'],
+    ['A2', 'B2'],
+    ['A3', 'B3'],
   ];
 
   return (
     <div className="container mt-[14px]">
-      <table className="w-[300px] bg-white md:w-[615px]">
-        <thead>
-          <tr className="bg-[#EFEFEF] text-[11px]">
-            <th className="font-medium p-2 text-black">Price</th>
-            <th className="font-medium p-2 text-black">Reward Rate</th>
-            <th className="font-medium p-2 text-black">Annual Reward in $</th>
+      <table className="w-[300px] bg-white md:w-full">
+        <thead className="">
+          <tr className="rounded w-full bg-[#EFEFEF] text-[11px]">
+            <th className="font-medium p-3 text-black">Price</th>
+            <th className="font-medium p-3 text-black">Reward Rate</th>
+            <th className="font-medium p-3 text-black">Annual Reward in $</th>
           </tr>
         </thead>
         <tbody>
           {data.map((row, index) => (
             <tr key={index}>
-              <td className="px-4 py-2">{row[0]}</td>
-              <td className="px-4 py-2">{row[1]}</td>
-              <td className="px-4 py-2">
-                <input
-                  className="border rounded px-2 py-1 focus:outline-none"
-                  type="text"
-                  placeholder="Enter formula"
-                />
-                <span className="ml-2">Result</span>
+              <td className="">
+                <div className="flex items-center justify-start rounded hover:shadow">
+                  <input
+                    className="border-r outline-none border-[#F5F5F5] text-[11px] text-black text-center w-full bg-[#FAFAFA] mt-2 py-4"
+                    type="text"
+                    value={row[0]}
+                  />
+                  <Image src={pencil} className="ml-[-1rem] mt-[1rem]" alt="Pencil edit icon" />
+                </div>
+              </td>
+              <td className="">
+                <div className="flex items-center justify-start rounded hover:shadow">
+                  <input
+                    className="border-r outline-none border-[#F5F5F5] text-[11px] text-black text-center w-full bg-[#FAFAFA] mt-2 py-4"
+                    type="text"
+                    value={row[1]}
+                  />
+                  <Image src={pencil} className="ml-[-1rem] mt-[1rem]" alt="Pencil edit icon" />
+                </div>
+              </td>
+              <td className="">
+                <div className="flex items-center justify-start rounded hover:shadow">
+                  <input
+                    className="w-full outline-none text-[11px] text-black text-center bg-[#FAFAFA] mt-2 py-4"
+                    type="text"
+                  />
+                  <Image src={pencil} className="ml-[-1rem] mt-[1rem]" alt="Pencil edit icon" />
+                </div>
               </td>
             </tr>
           ))}
