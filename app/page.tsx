5099478c3886b287e35e9527b9f4833fcc9d2134
puritybirir <puritybirir@gmail.com>
2023-06-25 16:24:53 +0300

    Add recoil store for state management

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import Cell from "@/components/Cell"
 import SearchBar from "@/components/SearchBar"
 import Table from "@/components/Table"
+import { RecoilRoot } from "recoil"
 
 export default function Home() {
   return (
@@ -8,7 +11,9 @@ export default function Home() {
       <h1 className="text-[1rem] mt-[34px] font-bold text-black md:text-[1.25rem]">Your Personal Staking Calculator</h1>
       <SearchBar />
       {/* <Table /> */}
-      <Cell>Hello</Cell>
+      <RecoilRoot>
+        <Cell>Hello</Cell>
+      </RecoilRoot>
     </main>
   )
 }
---
+++ components/store/CellContentState.ts
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const cellContent = atom({
+  key: 'cell',
+  default: 'fgrifhri'
+})
